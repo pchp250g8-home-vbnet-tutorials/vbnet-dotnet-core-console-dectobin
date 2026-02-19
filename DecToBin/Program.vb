8905009a1e54926b950ec1b45c839c6920d876fd
Alexander <pchp250g8@tutamail.com>
2026-02-19 13:42:50 +0300

    Добавьте файлы проекта.

---
+++ DecToBin/Program.vb
@@ -0,0 +1,30 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim uNumber As UInteger
+        Console.WriteLine("Input an unsigned integer number")
+        Dim bIsRightNumber = (UInteger.TryParse(Console.ReadLine(), uNumber)) AndAlso
+                     (uNumber <= UInteger.MaxValue)
+        If (Not bIsRightNumber) Then
+            Console.WriteLine("Invalid number format or number too big")
+            Console.Read()
+            Return
+        End If
+        Dim strBinNum = ""
+        Dim uTempVal = uNumber
+        Do While (uTempVal > 0)
+            strBinNum = CInt(uTempVal Mod 2).ToString() + strBinNum
+            uTempVal /= 2
+        Loop
+        If (strBinNum.Length = 0) Then
+            strBinNum = "0"
+        End If
+        Console.WriteLine _
+        (
+            "The binary equavalent of the decimal number {0} is: {1}",
+            uNumber, strBinNum
+        )
+        Console.Read()
+    End Sub
+End Module
